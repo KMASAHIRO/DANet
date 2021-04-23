fd5ac8b5407e3de5bee98e299e225cb9384f8718
KMASAHIRO <k.masa1215@gmail.com>
2021-04-23 20:13:11 +0900

    changed install_requires

--- DANet/__init__.py
+++ DANet/__init__.py
@@ -1,14 +1,5 @@
 import subprocess
 
-subprocess.run(['pip','install','tensorflow==2.4.1'])
-subprocess.run(['pip','install','museval==0.4.0'])
-subprocess.run(['pip','install','soundfile==0.10.3.post1'])
-subprocess.run(['pip','install','pandas==1.1.5'])
-subprocess.run(['pip','install','numpy==1.19.5'])
-subprocess.run(['pip','install','scipy==1.4.1'])
-subprocess.run(['pip','install','librosa==0.8.0'])
-subprocess.run(['pip','install','matplotlib==3.2.2'])
-
 from . import preprocess
 from . import generating_data
 from . import models
--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="DANet",
-    version="0.0.4",
+    version="0.0.5",
+    install_requires=["tensorflow>=2.4.1","museval>=0.4.0","soundfile>=0.10.3.post1",
+                      "pandas>=1.1.5","numpy>=1.19.5","scipy>=1.4.1","librosa>=0.8.0",
+                      "matplotlib>=3.2.2"],
     author="KMASAHIRO",
     description="a model that separates a mixed sound into whatever kinds of sound you like",
     long_description=long_description,
